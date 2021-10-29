fa693f21ed7515414ea3435f0e076baceca6bc38
tyesc <escolano.tyler@gmail.com>
2021-10-29 14:59:35 +0200

    fix: fix account useless

--- services/auth/index.js
+++ services/auth/index.js
@@ -2,7 +2,6 @@ import { Server } from '@job-guetter/api-core';
 
 import { MongoDB } from '@job-guetter/api-core/connectors';
 
-import * as Account from './Account';
 import * as Token from './Token';
 
 export default async ({ port } = {}) => {
@@ -13,12 +12,6 @@ export default async ({ port } = {}) => {
       MongoDB,
     ],
     routes: {
-
-      // Account
-      'POST /auth/register': {
-        interceptors: [],
-        handle: Account.register,
-      },
 
       // Token
       'POST /auth/login': {
