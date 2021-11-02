5f1d9f2bc2bf9f72ba0f1e71687016b3777764c4
tyesc <escolano.tyler@gmail.com>
2021-11-02 19:27:32 +0100

    refactor: clean code

--- services/general/Recruiter.js
+++ services/general/Recruiter.js
@@ -104,7 +104,7 @@ export const getAllCompanies = async (req, res) => {
     .find({ user })
     .populate('company', ['name', 'logo']);
 
-  const companies = recruiters.map(r => r.comapny);
+  const companies = recruiters.map(r => r.company);
 
   res.json({ companies });
 };
