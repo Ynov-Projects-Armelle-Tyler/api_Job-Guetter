1fd3f1bbd9503a10ed60eaa76f57cc6fa5d03fe8
CaramelleB <bengochearmelle@gmail.com>
2021-11-05 09:41:50 +0100

    add announcements routes

---
+++ services/general/Announcement.js
@@ -0,0 +1,91 @@
+import mongoose from 'mongoose';
+import isEmail from 'validator/lib/isEmail';
+
+import {
+  Account,
+  Recruiter,
+  Announcement,
+} from '@job-guetter/api-core/models';
+import { assert } from '@job-guetter/api-core/utils/assert';
+import {
+  BadRequest,
+  NotFound,
+  Unauthorized
+} from '@job-guetter/api-core/utils/errors';
+
+company,
+recruiter,
+name,
+activity_field,
+contract_type,
+localisation,
+job_start,
+job_description,
+missions,
+profile,
+salary,
+
+
+
+export const create = async (req, res) => {
+  const announcementInfo = assert(req.body.announcement, BadRequest('invalid_request'));
+  const recruiter = req.decoded.id;
+
+  const isCompanyRecruiter = await Recruiter.findOne({ recruiter, company })
+
+  if (!isCompanyRecruiter) {
+    Unauthorized('not_allowed')
+  }
+
+  const announcement = await Announcement.from({ announcementInfo });
+
+  await announcement.save();
+
+  res.json({ created: true });
+};
+
+export const get = async (req, res) => {
+  const announcementId = assert(req.params.id, BadRequest('wrong_announcement_id'),
+    val => mongoose.Types.ObjectId.isValid(val));
+
+  const announcement = assert(
+    await Announcement.findOne({ _id: announcementId }),
+    NotFound('announcement_not_found')
+  );
+
+  res.json({ announcement });
+};
+
+export const getAll = async (req, res) => {
+  const companies = await Announcement.find();
+  res.json({ companies });
+};
+
+export const update = async (req, res) => {
+  const announcementId = assert(req.params.id, BadRequest('wrong_announcement_id'),
+    val => mongoose.Types.ObjectId.isValid(val));
+  const announcementInfo = assert(req.body.announcement, BadRequest('invalid_request'));
+
+  const announcement = assert(
+    await Announcement.findOne({ _id: announcementId, recruiter: req.decoded._id }),
+    NotFound('announcement_not_found')
+  );
+
+  Object.assign(announcement, announcementInfo);
+
+  await announcement.save();
+
+  res.json({ updated: true });
+};
+
+export const remove = async (req, res) => {
+  const announcementId = assert(req.params.id, BadRequest('wrong_announcement_id'),
+    val => mongoose.Types.ObjectId.isValid(val));
+
+  assert(
+    await Announcement.findOneAndUpdate({ _id: announcementId, deleted: true }),
+    NotFound('announcement_not_found')
+  );
+
+  res.json({ deleted: true });
+};
