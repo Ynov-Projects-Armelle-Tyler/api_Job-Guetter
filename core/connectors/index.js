b2304255e002a691923793f6019e721043a616de
tyesc <escolano.tyler@gmail.com>
2021-11-17 18:47:04 +0100

    feat(auth): add express-brute with redis

--- core/connectors/index.js
+++ core/connectors/index.js
@@ -1,3 +1,5 @@
 export { default as MongoDB } from './MongoDB';
 export { default as Sendgrid } from './Sendgrid';
 export { default as SireneAPI } from './SireneAPI';
+export { default as Brute } from './Brute';
+export { default as Redis } from './Redis';
