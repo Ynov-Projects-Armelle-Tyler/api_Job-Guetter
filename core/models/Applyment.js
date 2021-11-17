fba9b241c67d00d970a73c28df1b434eb82246d0
tyesc <escolano.tyler@gmail.com>
2021-11-17 14:14:13 +0100

    fix: use good model name

--- core/models/Applyment.js
+++ core/models/Applyment.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const JobApplyement = new mongoose.Schema({
+const Applyment = new mongoose.Schema({
 
   jobber: {
     type: mongoose.Schema.Types.ObjectId,
@@ -38,8 +38,8 @@ const JobApplyement = new mongoose.Schema({
   },
 });
 
-JobApplyement.statics.from = function (opts) {
+Applyment.statics.from = function (opts) {
   return new this(opts);
 };
 
-export default mongoose.model('JobApplyement', JobApplyement);
+export default mongoose.model('Applyment', Applyment);
