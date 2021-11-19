8b2fc5c6f532dba3f06aa52bd38b583711e1789f
tyesc <escolano.tyler@gmail.com>
2021-11-19 10:53:52 +0100

    tests: increase coverage

--- tests/services/general/Recruiter.test.js
+++ tests/services/general/Recruiter.test.js
@@ -42,6 +42,41 @@ describe('@job-guetter/api-general/Recruiter', () => {
       await user.remove();
     });
 
+    test('should not create a account because already exists', async () => {
+      const account = await Account
+        .from({
+          email: 't1@gmail.com',
+          password: 'test',
+          type: 'TYPE_RECRUITER',
+        }).save();
+
+      let err;
+
+      try {
+        await post(server, {
+          url: '/api/v1/general/recruiter',
+          body: {
+            recruiter: {
+              email: 't1@gmail.com',
+              password: 'test',
+              first_name: 'Patt updated',
+              last_name: 'Rik update',
+            },
+          },
+          // headers: {
+          //   ...getAuthorizationHeaders(data.authCode.access_token),
+          // },
+        });
+      } catch (e) {
+        err = e;
+      }
+
+      expect(err.statusCode).toBe(409);
+      expect(err.error.error).toBe('already_exists');
+
+      await account.remove();
+    });
+
   });
 
   afterAll(async () => {
